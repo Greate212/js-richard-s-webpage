0ff306dae0c5364f0ba00debdafab983c71674f1
Richard Goode <rgoode494@gmail.com>
2026-01-21 00:26:04 -0600

    UPDATE: Exercise 1 and 2  code added

---
+++ site.js
@@ -0,0 +1,9 @@
+const mainContentSection = document.querySelector('h2')
+mainContentSection.style.color = 'red'
+
+
+const footerInstructions = document.querySelector('footer a')
+footerInstructions.style.textDecoration = 'underline'
+
+const footerInstructionText = document.querySelector('#foot a')
+footerInstructionText.style.color = 'red'
