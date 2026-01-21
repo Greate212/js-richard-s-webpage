e9d610dd4323a8da0000bd22fadc7a88c9f355e2
Richard Goode <rgoode494@gmail.com>
2026-01-21 00:28:31 -0600

    UPDATE: Finishing touches

--- site.js
+++ site.js
@@ -1,9 +1,10 @@
+// Exercise 1
 const mainContentSection = document.querySelector('h2')
 mainContentSection.style.color = 'red'
 
-
+// Exercise 2
 const footerInstructions = document.querySelector('footer a')
 footerInstructions.style.textDecoration = 'underline'
 
 const footerInstructionText = document.querySelector('#foot a')
-footerInstructionText.style.color = 'red'
+footerInstructionText.style.color = 'underline'
